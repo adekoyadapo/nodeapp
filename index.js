c18223acf536975b7891796d00e3c64e350c9db9
Ade <49321398+adekoyadapo@users.noreply.github.com>
2022-01-03 05:21:09 -0700

    update index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('This is the Main!'))
+app.get('/', (req, res) => res.send('This is the Main Deployment!'))
 
 app.listen(3000, () => console.log('Node.js app listening on port 3000.'))
