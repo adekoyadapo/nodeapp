3a58861ec4cf9f9b925c3718f839293058c353d9
adekoyadapo <49321398+adekoyadapo@users.noreply.github.com>
2022-02-11 09:01:09 -0700

    Nodeapp: Updated with API, changed workflow steps

--- README.md
+++ README.md
@@ -4,3 +4,39 @@
 
 A demo project to deploy a nodejs app with testing and azure pipeline.
 
+## 2020-02
+### Improvements
+- Updated with /healthz endpoint to allow for testing on k8s platforms
+- Updated with api endpoints that supports PUT, POST, DELETE, GET on path /api/courses
+
+### Usage
+- GET
+```
+curl http://localhost/api/courses
+...
+[{"id":1,"name":"course1"},{"id":2,"name":"course2"},{"id":3,"name":"course3"}]
+
+curl http://localhost/healthz
+...
+Healthy
+```
+- POST
+```
+curl -X POST http://localhost:/api/courses \
+   -H 'Content-Type: application/json' \
+   -d '{"name":"course4"}'
+...
+{"id":4,"name":"course4"}
+```
+- PUT
+```
+curl -X PUT http://localhost:/api/courses/1 \
+   -H 'Content-Type: application/json' \
+   -d '{"name":"course-test"}'
+...
+{"id":1,"name":"course-test"}
+```
+- DELETE
+```
+curl -X DELETE http://localhost:/api/courses/1  
+```
--- index.js
+++ index.js
@@ -1,16 +1,77 @@
-    // importing express framework
-    const express = require("express");
+// importing express framework
+const express = require("express");
+const Joi = require('joi')
+const res = require("express/lib/response");
+const app = express();
 
-    const app = express();
+app.use(express.json())
 
-    // Respond with "hello world" for requests that hit our root "/"
-    app.get("/", function (req, res) {
-     return res.send("Hello World");
-    });
+const courses = [
+  {id: 1, name: 'course1'},
+  {id: 2, name: 'course2'},
+  {id: 3, name: 'course3'},
+];
+// Respond with "hello world" for requests that hit our root "/"
+app.get("/", function (req, res) {
+ return res.send("Hello World");
+});
 
-    // listen to port 7000 by default
-    app.listen(process.env.PORT || 3000, () => {
-      console.log("Server is running");
-    });
+app.get("/healthz", function (req, res) {
+  return res.send("Healthy");
+ });
+ 
+app.get('/api/courses' , (req, res) => {
+  res.send(courses);
+  });
 
-    module.exports = app;
+
+app.get('/api/courses/:id', (req, res) => {
+ const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send('Course not found');
+  res.send(course);
+});
+
+app.post('/api/courses', (req, res) => {
+  const { error }= validateCourse(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+app.put('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send('Course ID not found');
+  
+  const { error }= validateCourse(req.body);
+  if (error) return res.status(400).send(error.details[0].message)
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
+function validateCourse(course){
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+  return result = Joi.validate(course, schema);
+
+}
+app.delete('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send('Course ID not found');
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.send(course);
+});
+// listen to port 3000 by default
+const port= process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+})
+module.exports = app;
