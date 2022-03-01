d065f77035c687d72c80b03fa18b97cee189bce9
adekoyadapo <49321398+adekoyadapo@users.noreply.github.com>
2022-03-01 16:16:38 -0700

    nodeapp: added more api items, updated docker-ci tags

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const courses = [
   {id: 1, name: 'course1'},
   {id: 2, name: 'course2'},
   {id: 3, name: 'course3'},
+  {id: 4, name: 'course4'},
+  {id: 5, name: 'course5'},
+  {id: 6, name: 'course6'},
 ];
 // Respond with "hello world" for requests that hit our root "/"
 app.get("/hello", function (req, res) {
